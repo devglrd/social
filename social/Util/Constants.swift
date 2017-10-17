e7713268d2f645c5c4f9ddccf8a65f19735b10e0
Remi Guillard <glrd.remi@gmail.com>
2017-10-17 10:09:32 +0200

    UI deesign

---
+++ social/Util/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  social
+//
+//  Created by Remi Guillard on 17/10/2017.
+//  Copyright © 2017 Remi Guillard. All rights reserved.
+//
+
+import UIKit
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
